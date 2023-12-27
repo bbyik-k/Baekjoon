393737b87f590792d8ec4dddc2a06654077587f3
bbyik <47810773+bbyik-k@users.noreply.github.com>
2023-12-27 21:34:22 +0900

    [Bronze III] Title: 알람 시계, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,17 +1,18 @@
-let input = require('fs').readFileSync('dev/stdin').toString().split(' ');
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-let hour = Number(input[0]);  // Hour
-let minute = Number(input[1]);  // Minute
+let hour = Number(input[0]);
+let minute = Number(input[1]);
 
 minute -= 45;
 
-if (minute < 0) {
-    minute += 60;
-    hour--;
-
-    if (hour === -1) {
-        hour = 23;
-    }
+if(minute < 0){
+  minute += 60;
+  hour--;
 }
 
-console.log(hour + ' ' + minute);
+if(hour < 0){
+  hour += 24
+}
+
+console.log(`${hour} ${minute}`);
