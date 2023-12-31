dba3cc1992eded8158d00bf05f9b5edd04d8f650
bbyik-k <kby00517@naver.com>
2023-12-31 20:26:54 +0900

    Add queue algorithm

--- basic_algorithm/queue.js
+++ basic_algorithm/queue.js
@@ -4,6 +4,35 @@ class Queue {
     this.headIndex = 0;
     this.tailIndex = 0;
   }
-  // enqueue()
+  enqueue(item){
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue(){
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek(){
+    return this.items[this.headIndex];
+  }
+  getLength(){
+    return this.tailIndex - this.headIndex;
+  }
+}
 
+let queue = new Queue();
+
+queue.enqueue(5);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(7);
+queue.dequeue();
+queue.enqueue(1);
+queue.enqueue(4);
+queue.dequeue();
+
+while(queue.getLength() != 0){
+  console.log(queue.dequeue());
 }
