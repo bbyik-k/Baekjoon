d1be652adac4a45409e6393e67eb3e9b23f400a2
bbyik-k <kby00517@naver.com>
2024-01-01 23:44:58 +0900

    Add heap

---
+++ basic_algorithm/heap/heap.js
@@ -0,0 +1,13 @@
+const PriorityQueue = require(".");
+
+let pq = new PriorityQueue(function(a, b){
+  return a.cash - b.cash;
+});
+
+pq.enq({cash: 250, name: 'Doohyun Kim'});
+pq.enq({cash: 300, name: 'Gildong Hong'});
+pq.enq({cash: 150, name: 'Minchul Han'});
+console.log(pq.size()); // 3
+console.log(pq.deq()); // {cash: 300, name: 'Gildong Hong'}
+console.log(pq.peek()); // {cash: 250, name: 'Doohyun Kim'}
+console.log(pq.size()); // 2
