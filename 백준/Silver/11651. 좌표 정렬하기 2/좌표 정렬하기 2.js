319ef58d919119b1f697c892e3816924ccc9bec7
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-01-03 10:04:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 436 ms, Memory: 52936 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
+
+input.shift();
+
+let numArr = input.map((num) => num.split(' ').map(Number));
+
+numArr.sort(sorting);
+
+function sorting(a, b) {
+  return a[1] - b[1] || a[0] - b[0];
+}
+
+let resultArr = numArr.map((ele) => `${ele[0]} ${ele[1]}`);
+console.log(resultArr.join('\n'));
