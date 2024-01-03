7350a7de7ed33070d156f7db68d7560395463919
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-01-03 13:14:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 456 ms, Memory: 60944 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -5,8 +5,9 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 input.shift();
 
 let arr = input.map((ele) => ele.split(' '));
-arr = arr.map((ele, idx) => [Number(ele[0]), ele[1], idx]);
+arr = arr.map((ele) => [Number(ele[0]), ele[1]]);
 
-arr.sort((a, b) => a[0] - b[0] || a[2] - b[2]);
+//nodejs 정렬은 기본값 stable
+arr.sort((a, b) => a[0] - b[0]);
 arr = arr.map((ele) => [`${ele[0]} ${ele[1]}`]);
 console.log(arr.join('\n'));
