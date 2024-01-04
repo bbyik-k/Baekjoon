fd25bb5ed3e26300e782b6db51acb9b728293e88
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-01-04 23:45:13 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -2,20 +2,18 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
 
+let len = Number(input[0].split(' ')[0]);
 let target = Number(input[0].split(' ')[1]);
 
-let moneyArr = [...input].map(Number);
-moneyArr.shift();
-let total = 0;
-let cnt = 0;
+let moneyArr = input.map(Number);
 
-moneyArr.reverse().forEach((ele) => {
-  if (target === 0) return;
-  if (target >= ele) {
-    cnt = parseInt(target / ele);
-    target -= cnt * ele;
-    total += cnt;
-  }
-});
+let total = 0;
+
+for (let i = len; i > 0; i--) {
+  if (target === 0) break;
+  let count = Math.floor(target / moneyArr[i]); // 해당 동전을 몇 개 사용해야 하는지
+  total += count;
+  target -= count * moneyArr[i];
+}
 
 console.log(total);
