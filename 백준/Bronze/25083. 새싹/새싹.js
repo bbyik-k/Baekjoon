1739698789c31c47863ab9a06991f2875438a78e
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-01-21 23:11:57 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 9276 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,10 @@
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
+
+console.log('         ,r\'"7');
+console.log("r`-_   ,'  ,/");
+console.log(' \\. ". L_r\'');
+console.log('   `~\\/');
+console.log('      |');
+console.log('      |');
