ad0b00568cbe4ff9fdfce997e7077ad5b52c2243
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-10-30 23:52:55 +0900

    [Silver II] Title: 나무 자르기, Time: 512 ms, Memory: 137028 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -1,28 +1,30 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-//let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
+// let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
 
-const [n, target] = input[0].split(' ').map(Number);
-const treeArr = input[1].split(' ').map(Number);
+let [cnt, target] = input[0].split(' ').map(Number);
+let treesArr = input[1].split(' ').map(Number);
 
 let min = 0;
-let max = Math.max(...treeArr);
+let max = Math.max(...treesArr);
+
 let result = 0;
 
 while (min <= max) {
   let mid = parseInt((min + max) / 2);
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    if (mid < treeArr[i]) {
-      sum += treeArr[i] - mid;
-    }
+  let total = 0;
+
+  for (let i = 0; i < cnt; i++) {
+    let diff = treesArr[i] - mid;
+    total += diff > 0 ? diff : 0;
   }
 
-  if (sum >= target) {
+  if (total < target) {
+    max = mid - 1;
+  } else if (total >= target) {
     result = mid;
     min = mid + 1;
-  } else {
-    max = mid - 1;
   }
 }
+
 console.log(result);
