d0a5bc6080579f26c0b93cb714fb1dd4dae2d2cb
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-11-03 23:38:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 640 ms, Memory: 161580 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -2,10 +2,8 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
 
-let cardNum = Number(input[0]);
 let cardArr = input[1].split(' ').map(Number);
 
-let targetNum = Number(input[2]);
 let targetArr = input[3].split(' ').map(Number);
 
 let cardMap = new Map();
