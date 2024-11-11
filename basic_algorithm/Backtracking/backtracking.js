c9465addde9b46a9db8dfbbd6953155de27139ed
bbyik-k <kby00517@naver.com>
2024-11-11 23:57:07 +0900

    FIx console

--- basic_algorithm/Backtracking/backtracking.js
+++ basic_algorithm/Backtracking/backtracking.js
@@ -2,9 +2,9 @@ let n = 4; // 전체 맵(map)의 크기
 let queens = []; // 현재 체스판에 놓인 퀸(queen)의 위치 정보들
 function possible(x, y) {
   // (x, y) 위치에 퀸을 놓을 수 있는지 확인
-  console.log(queens);
+  // console.log(queens);
   for (let [a, b] of queens) {
-    console.log(`a: ${a} x: ${x} / b: ${b} y: ${y}`);
+    // console.log(`a: ${a} x: ${x} / b: ${b} y: ${y}`);
     // 현재까지 놓았던 모든 퀸(queen)의 위치를 하나씩 확인하며
     if (a == x || b == y) return false; // 행이나 열이 같다면 놓을 수 없음
     if (Math.abs(a - x) == Math.abs(b - y)) return false; // 대각선에 위치한 경우 놓을 수 없음
@@ -18,7 +18,7 @@ function dfs(row) {
 
     return;
   }
-  row == n ? console.log(`--------------------------------------------------------------------------------------------row: ${row}`) : console.log(`-----------------------row: ${row}`);
+  // row == n ? console.log(`--------------------------------------------------------------------------------------------row: ${row}`) : console.log(`-----------------------row: ${row}`);
 
   for (let i = 0; i < n; i++) {
     // 현재 행(row)에 존재하는 열을 하나씩 확인하며
