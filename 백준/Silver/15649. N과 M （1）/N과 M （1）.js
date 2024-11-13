a0860b84f6b878491e8d656c5b1abdeada750e06
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-11-13 22:51:13 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 17424 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,19 +1,27 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
-let [n, m] = input[0].split(' ').map(Number); // 수열max, 수열 개수
-let prog = new Set();
-function findProgression(num) {
-  if (prog.size == m) {
-    console.log(...prog);
+let [n, m] = input[0].split(' ').map(Number);
+
+let visited = new Array(n + 1).fill(false); // 각 원소별 방문 여부 체크 배열
+let selected = []; // 현재 순열에 포함된 원소
+let answer = '';
+
+function dfs(depth) {
+  if (depth === m) {
+    answer += selected.join(' ') + '\n';
     return;
   }
+
   for (let i = 1; i <= n; i++) {
-    if (prog.has(i)) continue;
-    prog.add(i);
-    findProgression(num + 1);
-    prog.delete(i);
+    if (visited[i]) continue; // 이미 사용한 숫자는 스킵
+    selected.push(i); // 현재 숫자 추가
+    visited[i] = true; // 방문 처리
+    dfs(depth + 1); // 다음 깊이 탐색
+    selected.pop(); // 현재 숫자 제거 (백트래킹)
+    visited[i] = false; // 방문 처리 해제
   }
 }
 
-findProgression(1);
+dfs(0);
+console.log(answer);
