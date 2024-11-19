139e0d8e8094035e2d4d77909578da33ca122651
bbyik <47810773+bbyik-k@users.noreply.github.com>
2024-11-19 22:19:24 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 9444 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -2,28 +2,20 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
 
-let [n, m] = input[0].split(' ').map(Number);
-let resultArr = [];
-let checkArr = new Array(n).fill(false);
-// console.log(checkArr);
-let last = 1;
+let [n, m] = input[0].split(' ').map(Number); // N과 M 입력값
+let resultArr = []; // 현재 수열 저장
 
-function backtrak(depth) {
-  // console.log(`depth: ${depth}`);
-  if (depth > m) {
-    console.log(resultArr.join(' '));
+function backtrack(start, depth) {
+  if (depth === m) {
+    console.log(resultArr.join(' ')); // 수열 출력
     return;
   }
-  for (let i = last; i <= n; i++) {
-    // console.log(`i: ${i}`);
-    if (checkArr[i]) continue;
-    resultArr.push(i);
-    last = i;
-    checkArr[i] = true;
-    backtrak(depth + 1);
-    checkArr[i] = false;
-    resultArr.pop();
+
+  for (let i = start; i <= n; i++) {
+    resultArr.push(i); // 현재 숫자 추가
+    backtrack(i + 1, depth + 1); // 다음 숫자 선택
+    resultArr.pop(); // 현재 숫자 제거 (백트래킹)
   }
 }
 
-backtrak(1);
+backtrack(1, 0); // 시작값 1과 깊이 0으로 호출
