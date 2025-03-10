908011764f8ea1deacd8ec5ae083f1db0d3bf843
bbyik-k <kby00517@naver.com>
2025-03-11 00:17:27 +0900

    Nqueen 수정

--- 백준/Gold/9663. N－Queen/N－Queen.js
+++ 백준/Gold/9663. N－Queen/N－Queen.js
@@ -7,8 +7,8 @@ let cnt = 0;
 
 // 🔹 퀸 배치 여부를 빠르게 확인하는 배열 3개
 let cols = new Array(n).fill(false);
-let diag1 = new Array(2 * n - 1).fill(false); // x + y (↘ 대각선)
-let diag2 = new Array(2 * n - 1).fill(false); // x - y + (n - 1) (↙ 대각선)
+let diag1 = new Array(2 * n - 1).fill(false); // x - y + (n - 1)(↘ 대각선)
+let diag2 = new Array(2 * n - 1).fill(false); // x + y  (↙ 대각선)
 
 function dfs(row) {
   if (row === n) {
@@ -16,13 +16,13 @@ function dfs(row) {
     return;
   }
   for (let col = 0; col < n; col++) {
-    if (cols[col] || diag1[row + col] || diag2[row - col + (n - 1)]) continue; // 이미 사용된 경우 스킵
+    if (cols[col] || diag1[row - col] || diag2[row + col]) continue; // 이미 사용된 경우 스킵
 
     // 퀸 배치
-    cols[col] = diag1[row + col] = diag2[row - col + (n - 1)] = true;
+    cols[col] = diag1[row - col] = diag2[row + col] = true;
     dfs(row + 1);
     // 백트래킹: 원래 상태로 복구
-    cols[col] = diag1[row + col] = diag2[row - col + (n - 1)] = false;
+    cols[col] = diag1[row - col] = diag2[row + col] = false;
   }
 }
 
