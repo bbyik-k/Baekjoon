81f750b438798b58ebda08bb7429a4e0f3a654af
ByeongIk Kim <kby00517@naver.com>
2025-04-29 13:47:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 208 ms, Memory: 15500 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,34 +1,54 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
-const input = fs.readFileSync(path).toString().trim().split(' ').map(Number);
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
-const [N, K] = input;
+const MAX = 100001;
+let [n, k] = input[0].split(' ').map(Number); // 초기 위치(N)와 동생의 위치(K)
+let visited = new Array(MAX).fill(0); // 각 위치까지의 최단 시간
 
-const MAX = 100001; // 좌표 범위 (0 ~ 100,000)
-const visited = Array(MAX).fill(false);
-const distance = Array(MAX).fill(0);
-
-function bfs(start) {
-  const queue = [];
-  queue.push(start);
-  visited[start] = true;
-
-  while (queue.length) {
-    const now = queue.shift();
-
-    if (now === K) {
-      console.log(distance[now]);
-      return;
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue() {
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.headIndex];
+  }
+  getLength() {
+    return this.tailIndex - this.headIndex;
+  }
+}
+function bfs() {
+  // 너비 우선 탐색(BFS)
+  queue = new Queue();
+  queue.enqueue(n);
+  while (queue.getLength() != 0) {
+    // 큐가 빌 때까지 반복
+    let cur = queue.dequeue();
+    if (cur == k) {
+      // 동생의 위치에 도달한 경우
+      return visited[cur]; // 최단 시간 출력
     }
-
-    for (let next of [now - 1, now + 1, now * 2]) {
-      if (next >= 0 && next < MAX && !visited[next]) {
-        queue.push(next);
-        visited[next] = true;
-        distance[next] = distance[now] + 1;
+    for (let nxt of [cur - 1, cur + 1, cur * 2]) {
+      // 공간을 벗어난 경우 무시
+      if (nxt < 0 || nxt >= MAX) continue;
+      // 아직 방문하지 않은 위치라면
+      if (visited[nxt] == 0) {
+        visited[nxt] = visited[cur] + 1;
+        queue.enqueue(nxt);
       }
     }
   }
 }
-
-bfs(N);
+console.log(bfs());
