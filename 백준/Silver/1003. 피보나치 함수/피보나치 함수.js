067541df7a1f00b4f26a29c88677d47d8d0cf5b7
ByeongIk Kim <kby00517@naver.com>
2025-07-12 15:25:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 88 ms, Memory: 9756 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,29 +1,26 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
-const input = fs.readFileSync(path).toString().trim().split('\n').map(Number);
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
-const T = input[0]; // 테스트 케이스 개수
-const testCases = input.slice(1);
+let n = Number(input[0]);
+let MAX = 41; //최대 피보나치는 40까지
 
-const MAX = 41; // N은 최대 40까지
+//dp 테이블 생성
+let dp = Array.from({ length: MAX }, () => [0, 0]); //[0출력 횟수, 1출력 횟수]
 
-// 0과 1의 출력 횟수를 저장하는 DP 배열
-const count0 = Array(MAX).fill(0);
-const count1 = Array(MAX).fill(0);
+//초기항 지정
+dp[0] = [1, 0];
+dp[1] = [0, 1];
 
-// 초기 조건
-count0[0] = 1;
-count1[0] = 0;
-count0[1] = 0;
-count1[1] = 1;
-
-// 점화식에 따라 DP 테이블 채우기
+//dp 테이블 값 입력
+// 3번째 항 dp[2] 부터 입력 시작
 for (let i = 2; i < MAX; i++) {
-  count0[i] = count0[i - 1] + count0[i - 2];
-  count1[i] = count1[i - 1] + count1[i - 2];
+  dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]];
 }
 
-// 테스트케이스 출력
-for (let n of testCases) {
-  console.log(`${count0[n]} ${count1[n]}`);
+// console.log(dp);
+
+for (let i = 1; i < n + 1; i++) {
+  const target = Number(input[i]);
+  console.log(`${dp[target][0]} ${dp[target][1]}`);
 }
