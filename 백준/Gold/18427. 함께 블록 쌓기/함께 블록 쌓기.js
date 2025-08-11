03bdcf26a23b91a07bf7919ea9435e20eb4b5b6b
ByeongIk Kim <kby00517@naver.com>
2025-08-11 10:34:58 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 216 ms, Memory: 20104 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.js
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.js
@@ -1,48 +1,44 @@
-// dp 유형 반복 학습할 것
-
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
 const input = fs.readFileSync(path).toString().trim().split('\n');
 
-//n: 학생수, m: 최대 블록수, h: 목표 탑 높이
-let [n, m, h] = input[0].split(' ').map(Number);
+const [N, M, H] = input[0].split(' ').map(Number);
 
-// 학생별 블록 정보
 let blocks = [];
-for (let i = 1; i <= n; i++) {
-  blocks.push(input[i].split(' ').map(Number));
+for (let i = 1; i <= N; i++) {
+  const line = input[i].split(' ').map(Number);
+  blocks.push(line);
 }
 
-// dp[h] = 높이 h를 만드는 경우의 수
-let dp = new Array(h + 1).fill(0);
-dp[0] = 1; //높이 0 인 경우는 아무것도 없는 경우 1가지
+let dp = new Array(H + 1).fill(0);
+dp[0] = 1; //0층(아무것도 없음) 상태의 경우의 수는 1
 
-// 모든 학생에 대하여 학생별로 ㄴ처리
-for (let i = 0; i < n; i++) {
-  // i = 학생 순서
-  let data = []; // 이번 학생의 블록을 써서 새로 만들 수 있는 (높이, 경우의 수) 저장
-
-  // 0부터 h까지의 모든 높이에 대하여 처리
-  for (let height = 0; height <= h; height++) {
+//1. 모든 학생에 대해 실행 1번~ 마지막 학생 N
+for (let i = 0; i < N; i++) {
+  let data = []; // 이번 학생의 블록을 사용해 만들 수 있는 [높이, 경우의 수] 저장
+  //2. 모든 높이에 대해 실행 0~ 목표높이 H
+  for (let height = 0; height <= H; height++) {
+    // 만들 수 없는 블록 층 생략
     if (dp[height] === 0) {
-      // 현재 높이를 만드는 방법이 없으면 패스
       continue;
     }
-    for (let k = 0; k < blocks.length; k++) {
-      // 해당 학생의 모든 블록을 확인하며
+    //3. i 번째 학생이 가지고 있는 모든 블록에 대해 실행 (최대 M개)
+    for (let curBlock of blocks[i]) {
       // 현재 학생의 블록으로 특정 높이의 탑을 쌓을 수 있는 경우
       // i 번째 학생의 k 번째 블록이 최대 높이를 넘지 않을 때
-      let curBlock = blocks[i][k];
-      if (height + curBlock <= h) {
-        data.push([height + curBlock, dp[height]]);
+      const newHeight = height + curBlock;
+      if (newHeight <= H) {
+        data.push([newHeight, dp[height]]);
       }
     }
   }
-  // console.log(data);
+
   // 쌓을 수 있는 높이에 대하여, 경우의 수 증가
+  // -> 이전 층의 경우의 수를 더하기
   // 중복 방지를 위해 한꺼번에 반영
   for ([newHeight, ways] of data) {
     dp[newHeight] = (dp[newHeight] + ways) % 10007;
   }
 }
-console.log(dp[h]);
+
+console.log(dp[H]);
