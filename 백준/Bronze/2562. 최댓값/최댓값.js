99029954067ebb48352b7b2b9f7437465b7a816c
ByeongIk Kim <kby00517@naver.com>
2026-01-19 09:03:30 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,8 +1,15 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n').map(Number);
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
-let max = Math.max(...input);
-let index = input.indexOf(max)+1;
+let numArr = [];
 
-console.log(max)
-console.log(index);
+for (let i = 0; i < 9; i++) {
+  numArr.push(Number(input[i]));
+}
+
+const max = Math.max(...numArr);
+const index = numArr.indexOf(max);
+
+console.log(max);
+console.log(index + 1);
