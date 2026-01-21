2c2d38e0710e2211fb3d9998c1d6b9edd6fd8aa3
ByeongIk Kim <kby00517@naver.com>
2026-01-21 09:14:12 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 13044 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -4,31 +4,39 @@ const input = fs.readFileSync(path).toString().trim().split('\n');
 
 const N = Number(input[0]);
 const stack = [];
+const out = []; //console log 매 번 출력하지 않고 모아서 출력 - 성능향상
 
 for (let i = 1; i <= N; i++) {
-  const line = input[i].split(' ');
-  const command = line[0];
+  const [command, value] = input[i].split(' ');
 
   switch (command) {
     case 'push':
-      const num = line[1];
-      stack.push(num);
+      stack.push(Number(value));
+      // 확장 가능성 - Number로 통일
+
       // console.log(`-------------push: ${stack}`);
       break;
     case 'pop':
-      console.log(stack.pop() || -1);
+      out.push(stack.length ? String(stack.pop()) : '-1');
+      //stack.pop() || -1 은 값에 0또는 공백 '', false 값이 들어왔을 떄 위험
+
       // console.log(`-------------pop: ${stack}`);
       break;
     case 'size':
-      console.log(stack.length);
+      out.push(String(stack.length));
+
       // console.log(`-------------size: ${stack}`);
       break;
     case 'empty':
-      console.log(stack.length === 0 ? 1 : 0);
+      out.push(stack.length === 0 ? '1' : '0');
+
       // console.log(`-------------empty: ${stack}`);
       break;
     case 'top':
-      console.log(stack[stack.length - 1] || -1);
+      // console.log(stack[stack.length - 1] || -1);
+      out.push(stack.length ? stack[stack.length - 1] : '-1');
+      //마찬가지로 0 false 공백 고려 || 사용 지양
+
       // console.log(`-------------top: ${stack}`);
       break;
     default:
@@ -38,3 +46,6 @@ for (let i = 1; i <= N; i++) {
   // console.log(command);
   // console.log(line.length);
 }
+
+//로그 모아서 한 번에 출력
+console.log(out.join('\n'));
