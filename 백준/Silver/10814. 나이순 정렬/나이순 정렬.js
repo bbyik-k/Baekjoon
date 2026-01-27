41d1a6f0cb2cc83fd92d19d3e17e39721028c6f8
ByeongIk Kim <kby00517@naver.com>
2026-01-27 14:01:38 +0900

    [Silver V] Title: 나이순 정렬, Time: 348 ms, Memory: 44160 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,13 +1,14 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-// let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
 input.shift();
+let memberArr = input.map((ele) => ele.split(' '));
 
-let arr = input.map((ele) => ele.split(' '));
-arr = arr.map((ele) => [Number(ele[0]), ele[1]]);
+// console.log(memberArr);
 
-//nodejs 정렬은 기본값 stable
-arr.sort((a, b) => a[0] - b[0]);
-arr = arr.map((ele) => [`${ele[0]} ${ele[1]}`]);
-console.log(arr.join('\n'));
+memberArr.sort((a, b) => a[0] - b[0]);
+memberArr = memberArr.map((ele) => ele.join(' '));
+// console.log(memberArr);
+
+console.log(memberArr.join('\n'));
