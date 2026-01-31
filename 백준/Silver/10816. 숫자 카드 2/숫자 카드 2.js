ffbdd773d6d29b8154385022504ba34c29ebd942
ByeongIk Kim <kby00517@naver.com>
2026-01-31 15:10:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 640 ms, Memory: 161580 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('./test.txt').toString().trim().split('\n');
+
+let cardArr = input[1].split(' ').map(Number);
+
+let targetArr = input[3].split(' ').map(Number);
+
+let cardMap = new Map();
+let resultArr = [];
+
+cardArr.forEach((ele) => {
+  if (cardMap.has(ele)) cardMap.set(ele, cardMap.get(ele) + 1);
+  else cardMap.set(ele, 1);
+});
+
+targetArr.forEach((ele) => {
+  if (cardMap.get(ele)) resultArr.push(cardMap.get(ele));
+  else resultArr.push(0);
+});
+console.log(resultArr.join(' '));
