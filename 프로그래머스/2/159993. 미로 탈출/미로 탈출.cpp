6fb5e4897c18effc16e7d04dbff4ab763ab7bb8b
bbyik-k <kby00517@naver.com>
2026-09-19 15:41:47 +0900

    [level 2] Title: 미로 탈출, Time: 0.31 ms, Memory: 4.79 MB

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -1,14 +1,13 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 
 using namespace std;
 
 int dy[4] = {-1, 1, 0, 0};
 int dx[4] = {0, 0, -1, 1};
 
-int bfs(vector<string>& maps, int sy, int sx, char target){
+int bfs(const vector<string>& maps, int sy, int sx, char target){
     int n = (int)maps.size(), m = (int)maps[0].size();
     vector<vector<int>> dist(n, vector<int>(m, -1));
     queue<pair<int, int>> q;
@@ -21,30 +20,20 @@ int bfs(vector<string>& maps, int sy, int sx, char target){
         
         for(int d=0; d<4; d++){
             int ny = y+dy[d], nx = x+dx[d];
-            // cout << "ny: " << ny << " nx: " << nx << '\n';
             
             if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
             if(dist[ny][nx] != -1) continue;
             if(maps[ny][nx] == 'X') continue;
             
             dist[ny][nx] = dist[y][x] + 1;
+            if(maps[ny][nx] == target){return dist[ny][nx];}
             q.push({ny, nx});
-            
-            // cout << "maps[ny][nx]: " << maps[ny][nx] << '\n';
-            // cout << "target: " << target << '\n';
-            
-            if(maps[ny][nx] == target){
-                // cout << 'target!' << '\n';
-                return dist[ny][nx];
-            }
-            
         }
     }
     return -1;
 }
 
 int solution(vector<string> maps) {
-    int answer = 0;
     int sizeY = (int)maps.size(), sizeX = (int)maps[0].size();
     
     int sy, sx, ly, lx; //시작점 yx, 레버 yx
@@ -58,28 +47,11 @@ int solution(vector<string> maps) {
     
     //레버까지 최단거리
     int toL = bfs(maps, sy, sx, 'L');
-    // cout << "toL: " << toL << '\n';
     if(toL == -1) return -1;
     
     //출구까지 최단거리
     int toE = bfs(maps, ly, lx, 'E');
     if(toE == -1) return -1;
-    
-    
-    
-    
-    
-    
-    
+   
     return toL + toE;
 }
-
-
-
-
-
-
-
-
-
-
