821e09b2debe8a2513ca7d75e0f187c550376bcd
ByeongIk Kim <kby00517@naver.com>
2026-09-21 13:24:50 +0900

    [level 2] Title: 미로 탈출, Time: 0.33 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <iostream>
 
 using namespace std;
 
@@ -12,46 +13,48 @@ int bfs(const vector<string>& maps, int sy, int sx, char target){
     vector<vector<int>> dist(n, vector<int>(m, -1));
     queue<pair<int, int>> q;
     
-    dist[sy][sx] = 0; //현재 위치는 0초
+    dist[sy][sx] = 0; //시작지점 0
     q.push({sy, sx});
     
     while(!q.empty()){
         auto [y, x] = q.front(); q.pop();
         
         for(int d=0; d<4; d++){
-            int ny = y+dy[d], nx = x+dx[d];
+            int ny = y + dy[d], nx = x + dx[d];
             
             if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
             if(dist[ny][nx] != -1) continue;
             if(maps[ny][nx] == 'X') continue;
             
             dist[ny][nx] = dist[y][x] + 1;
-            if(maps[ny][nx] == target){return dist[ny][nx];}
+            
+            if(maps[ny][nx] == target) {return dist[ny][nx];}
+            
             q.push({ny, nx});
         }
+        
+        
     }
     return -1;
 }
 
+
 int solution(vector<string> maps) {
-    int sizeY = (int)maps.size(), sizeX = (int)maps[0].size();
     
-    int sy, sx, ly, lx; //시작점 yx, 레버 yx
-    
-    for(int i=0; i<sizeY; i++){
-        for(int j=0; j<sizeX; j++){
-            if(maps[i][j] == 'S'){sy = i; sx = j;}
-            if(maps[i][j] == 'L'){ly = i; lx = j;}
+    int sy, sx, ly, lx;
+    int n = (int)maps.size(), m = (int)maps[0].size();
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(maps[i][j] == 'S') {sy=i; sx=j;}
+            if(maps[i][j] == 'L') {ly=i; lx=j;}
         }
     }
     
-    //레버까지 최단거리
     int toL = bfs(maps, sy, sx, 'L');
-    if(toL == -1) return -1;
+    if(toL == -1) {return -1;}
     
-    //출구까지 최단거리
     int toE = bfs(maps, ly, lx, 'E');
-    if(toE == -1) return -1;
-   
+    if(toE == -1) {return -1;}
+    
     return toL + toE;
 }
